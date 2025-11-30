75e5e1251b6ffe105d4896cf96209af78d228214
Udot029 <udot029@gmail.com>
2025-11-30 18:21:18 +0530

    Add files via upload

---
+++ Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PlaceOrder from '../components/placeorder.jsx'
+
+const Orders = () => {
+  return (
+    <div className='orders-page'>
+      <PlaceOrder />
+    </div>
+  )
+}
+
+export default Orders
